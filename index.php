d6b641fd4d4c38b7b46d4e59ed298cb91e9d8310
AndMJ <andrejoao070@gmail.com>
2021-12-31 16:49:51 +0000

    altera teste

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Consultas LDA</title>
+  <title>Consultas LDA ALTADVA</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
