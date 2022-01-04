34266aeb0009feaf6cde2d3adc356fcf7a4ee141
AndMJ <andrejoao070@gmail.com>
2022-01-04 21:07:55 +0000

    alinhamento clinica contactos

--- clinica.php
+++ clinica.php
@@ -36,103 +36,28 @@
 <body>
 
   <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+  <header id="header" class="header fixed-top d-flex">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="static/img/logo.png" alt="">
-        <span class="d-none d-lg-block">Consultas LDA</span>
-      </a>
-      <i class="bi bi-list toggle-sidebar-btn"></i>
+        <a href="index.html" class="logo d-flex align-items-center">
+            <img src="static/img/logo.png" alt="">
+            <span class="d-none d-lg-block">Consultas LDA</span>
+        </a>
     </div><!-- End Logo -->
-
-    <div class="search-bar">
-      <form class="search-form d-flex align-items-center" method="POST" action="#">
-        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-      </form>
-    </div><!-- End Search Bar -->
-
-    <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
-
-        <li class="nav-item d-block d-lg-none">
-          <a class="nav-link nav-icon search-bar-toggle " href="#">
-            <i class="bi bi-search"></i>
-          </a>
-        </li><!-- End Search Icon-->
-
-        <li class="nav-item dropdown pe-3">
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="static/img/admin.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">Admin</span>
-          </a><!-- End Profile Iamge Icon -->
-
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-              <h6>Admin</h6>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
-            </li>
-          </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
-
-      </ul>
-    </nav><!-- End Icons Navigation -->
 
   </header><!-- End Header -->
 
-  <!-- ======= Sidebar ======= -->
-  <aside id="sidebar" class="sidebar">
-    <ul class="sidebar-nav" id="sidebar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">
-          <i class="bi bi-grid"></i>
-          <span>Página principal</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
-      <li class="nav-item">
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="consultas.php">
-          <i class="bi bi-archive"></i>
-          <span>Consultas</span>
-        </a>
-      </li><!-- End Consultas Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="utentes.php">
-          <i class="bi bi-person"></i>
-          <span>Utentes</span>
-        </a>
-      </li><!-- End Clientes Page Nav -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="medicos.php">
-          <i class="bi bi-activity"></i>
-          <span>Médicos</span>
-        </a>
-      </li><!-- End Médicos Page Nav -->
-    </ul>
-  </aside><!-- End Sidebar-->
-
-  <main id="main" class="main">
-  <div class="row mb-2 mb-xl-3">
-  <div class="col-auto d-none d-sm-block">
+  <main id="clinica_main">
+  <div class="row">
+  <div class="col-12 text-center d-none d-sm-block">
     <h1>Contactos</h1>
   </div><!-- End Page Title -->
 
     <section class="section">
-
       <div class="container">
         <!-- Recent medical appointments -->
         <div class="col-12">
-          <div class="card recent-sales">
+          <div class="card">
             <div class="card-body">
                 <div class="row">
                     <div class="col-6">
@@ -175,119 +100,11 @@
         </div><!-- End Recent medical appointments -->
       </div>
       </div>
-
-    
-
-
-      <!-- Add Modal HTML -->
-      <div id="addEmployeeModal" class="modal fade">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <form id="user_form">
-              <div class="modal-header">
-                <h4 class="modal-title">Adicionar Utente</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-              </div>
-              <div class="modal-body">
-                <div class="form-group">
-                  <label>Nome</label>
-                  <input type="text" id="name" name="name" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>EMAIL</label>
-                  <input type="email" id="email" name="email" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>PHONE</label>
-                  <input type="phone" id="phone" name="phone" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>CITY</label>
-                  <input type="city" id="city" name="city" class="form-control" required>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <input type="hidden" value="1" name="type">
-                <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
-                <button type="button" class="btn btn-primary" id="btn-add">Adicionar</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
-
-
-      <!-- Edit Modal HTML -->
-      <div id="editEmployeeModal" class="modal fade">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <form id="update_form">
-              <div class="modal-header">
-                <h4 class="modal-title">Edit User</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-              </div>
-              <div class="modal-body">
-                <input type="hidden" id="id_u" name="id" class="form-control" required>
-                <div class="form-group">
-                  <label>Name</label>
-                  <input type="text" id="name_u" name="name" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>Email</label>
-                  <input type="email" id="email_u" name="email" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>PHONE</label>
-                  <input type="phone" id="phone_u" name="phone" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label>City</label>
-                  <input type="city" id="city_u" name="city" class="form-control" required>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <input type="hidden" value="2" name="type">
-                <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-                <button type="button" class="btn btn-info" id="update">Update</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
-
-
-      <!-- Delete Modal HTML -->
-      <div id="deleteEmployeeModal" class="modal fade">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <form>
-
-              <div class="modal-header">
-                <h4 class="modal-title">Delete User</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-              </div>
-              <div class="modal-body">
-                <input type="hidden" id="id_d" name="id" class="form-control">
-                <p>Are you sure you want to delete these Records?</p>
-                <p class="text-warning"><small>This action cannot be undone.</small></p>
-              </div>
-              <div class="modal-footer">
-                <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-                <button type="button" class="btn btn-danger" id="delete">Delete</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
     </section>
-
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+  <footer id="clinica_footer">
     <div class="copyright">
       &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
